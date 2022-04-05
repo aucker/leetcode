9e8908f41d9bc660fb49a3869ff56b6c1e3b1185
aucker <aucker@qq.com>
2022-04-05 12:03:45 +0800

    config the cargo.toml

---
+++ target/debug/deps/spiral_matrix-11aebc5ed28617b0.d
@@ -0,0 +1,5 @@
+/Users/aucker/Desktop/LeetCode_Solution/target/debug/deps/spiral_matrix-11aebc5ed28617b0.rmeta: spiral-matrix/src/lib.rs
+
+/Users/aucker/Desktop/LeetCode_Solution/target/debug/deps/spiral_matrix-11aebc5ed28617b0.d: spiral-matrix/src/lib.rs
+
+spiral-matrix/src/lib.rs:
