4e8f4eddb7d993bb44d8b25647f9b22e6083204a
aucker <aucker@qq.com>
2023-11-20 16:17:48 +0800

    Nov-20: min amount time collect garbage [M]

---
+++ daily/cpp/Nov-20-Min-amount-time-collect-garbage.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+    int len = garbage.size();
+    int res = 0;
+    bool m = false, g = false, m = false;
+    for (int i = len-1; i >= 0; i--) {
+      if (!g && garbage[i].find('G') != string::npos) {
+        g = true;
+        res += accumulate(travel.begin(), travel.begin() + i, 0);
+      }
+      if (!m && garbage[i].find('M') != string::npos) {
+        m = true;
+        res += accumulate(travel.begin(), travel.begin() + i, 0);
+      }
+      if (!p && garbage[i].find('P') != string::npos) {
+        p = true;
+        res += accumulate(travel.begin(), travel.begin() + i, 0);
+      }
+
+      if (m && g && p) {
+        break;
+      }
+    }
+
+    return res + accumulate(garbage.begin(), garbage.end(), 0, [](int sum, const string& str) {
+        return sum + str.size();
+    });
+  }
+}
