f6e25993e3bdb2cb02a757824107857a7da24539
aucker <aucker@qq.com>
2023-11-21 10:06:29 +0800

    Nov-21: Count nice pairs in an arrya [M] cpp and python

---
+++ daily/cpp/Nov-21-Count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  int reverse(int num) {
+    int rev = 0;
+    while (num > 0) {
+      rev = rev * 10 + num % 10;
+      num /= 10;
+    }
+    return rev;
+  }
+
+  int countNicePairs(vector<int>& nums) {
+    const int mod = 1e9 + 7;
+    int len = nums.size();
+    for (int i = 0; i < len; i++) {
+      nums[i] = nums[i] - reverse(nums[i]);
+    }
+
+    sort(nums.begin(), nums.end());
+    long res = 0;
+    for (int i = 0; i < len - 1; i++) {
+      long cont = 1;
+      while (i < len - 1 && nums[i] == nums[i + 1]) {
+        cont ++;
+        i++;
+      }
+      res = (res % mod + (cont * (cont - 1)) / 2) % mod;
+    }
+    return static_cast<int>(res % mod);
+  }
+}
