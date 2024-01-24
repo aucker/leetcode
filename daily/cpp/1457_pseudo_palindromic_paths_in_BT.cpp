21033e7d1659b6a35faade5c4fea7f244d755b89
aucker <aucker@qq.com>
2024-01-25 00:47:29 +0800

    update: 1457: Pseudo palindromic paths in a Binary Tree [M][DFS]
    DFS to traverse a binary tree

---
+++ daily/cpp/1457_pseudo_palindromic_paths_in_BT.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+  int root;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+}
+
+class Solution {
+public:
+  int pseudoPalindromicPaths(TreeNode* root) {
+    return cntPseudoPaths(root, 0);
+  }
+
+private:
+  int cntPseudoPaths(TreeNode* node, int path) {
+    if (!node) return 0;
+
+    path ^= (1 << node->val);
+    if (!node->left && !node->right) {
+      return (path & (path - 1)) == 0 ? 1 : 0;
+    }
+
+    return cntPseudoPaths(node->left, path) + cntPseudoPaths(node->right, path);
+  }
+};
