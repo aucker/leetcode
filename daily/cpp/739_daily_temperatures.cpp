0ff4babd94a4e93da1a878db0febc86a8b6422f9
aucker <aucker@qq.com>
2024-01-31 23:28:45 +0800

    739: daily temperatures [M]
    use stack is a better solution
    
    use 2 pointers will cause time exceeding.

---
+++ daily/cpp/739_daily_temperatures.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> temperature(vector<int> &T) {
+    vector<int> res(T.size(), 0);
+    stack<int> st;
+    for (int i = 0; i < T.size(); ++i) {
+      while (!st.empty() && T[i] > T[st.top()]) {
+        int idx = st.top();
+        st.pop();
+        res[idx] = i - idx;
+      }
+      st.push(i);
+    }
+    return res;
+  }
+
+  void test1() {
+    vector<int> T = {73, 74, 75, 71, 69, 72, 76, 73};
+    vector<int> res = temperature(T);
+    for (int i = 0; i < res.size(); ++i) {
+      cout << res[i] << " ";
+    }
+    cout << endl;
+  }
+};
+
+int main() {
+  Solution *s = new Solution();
+  s->test1();
+  return 0;
+}
