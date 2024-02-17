b2acecfa66502736ebb5bde99ff9a57efd38c24a
aucker <aucker@qq.com>
2024-02-17 22:23:38 +0800

    1642: furthest building you can reach [M][PQ]
    like greedy, use priority queue to solve this problem

---
+++ daily/cpp/1642_furthest_building_you_can_reach.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int furthestBuilding(vector<int> &heights, int bricks, int ladders) {
+    // Priority Queue for storing the bricks used in decreasing order
+    priority_queue<int> pq;
+
+    int i = 0, diff = 0;
+    for (i; i < heights.size() - 1; ++i) {
+      diff = heights[i + 1] - heights[i];
+
+      // clang-format off
+      if (diff <= 0) continue;
+
+      bricks -= diff;
+      pq.push(diff);
+      if (bricks < 0) {
+        bricks += pq.top();
+        pq.pop();
+        --ladders;
+      }
+
+      if (ladders < 0) break;
+    }
+    return i;
+  }
+};
