2e91bb2927de2984f50493554de891a328892baf
aucker <aucker@qq.com>
2024-03-12 13:08:33 +0800

    3077: max strength of k disjoin subarrays [H]
    DP of 3-ary

--- daily/cpp/3077_max_strength_of_k_disjoin_subarrays.cpp
+++ daily/cpp/3077_max_strength_of_k_disjoin_subarrays.cpp
@@ -12,6 +12,45 @@ class Solution {
     return solve(0, (ll)k, false, nums);
   }
 
+  ll maximumStrength1(vector<int>& nums, int k) {
+    dp = vector<vector<vector<ll>>>(
+        nums.size(), vector<vector<ll>>(2, vector<ll>(k + 1, -1e16)));
+    ll ans = -1e15;
+    return solve_optimize(0, (ll)k, false, nums);
+  }
+
+  ll maximumStrengthOptimize(vector<int>& nums, int k) {
+    ll sum[10005];
+    ll cur[10005], last[10005];
+    memset(cur, LLONG_MIN, sizeof(cur));
+    memset(last, LLONG_MIN, sizeof(last));
+    sum[0] = 0;
+    int n = nums.size();
+    for (int i = 0; i < n; ++i) sum[i + 1] = sum[i] + nums[i];
+
+    int flag = 1;
+    for (int u = 0; u < k; ++u) {
+      int w = (k - u) * flag;
+      memcpy(last, cur, sizeof(cur));
+      for (int i = u; i <= n; ++i) {
+        cur[i] = LLONG_MIN;
+      }
+
+      ll tmp = LLONG_MIN;
+      for (int i = u; i <= n; ++i) {
+        if (i) cur[i] = max(cur[i], cur[i - 1]);
+        if (tmp != LLONG_MIN) {
+          cur[i] = max(cur[i], tmp + sum[i] * w);
+        }
+        if (last[i] != LLONG_MIN) {
+          tmp = max(tmp, last[i] - sum[i] * w);
+        }
+      }
+      flag = -flag;
+    }
+    return cur[n];
+  }
+
  private:
   vector<vector<vector<ll>>> dp;
 
@@ -38,5 +77,29 @@ class Solution {
     }
 
     return dp[i][k][started] = ans;
+  }
+
+  ll solve_optimize(int i, ll k, bool started, vector<int>& nums) {
+    if (k == 0) return 0;
+    if (i == nums.size()) {
+      if (k == 1 && started) return 0;
+      return -1e15;
+    }
+    if (dp[i][started][k] != -1e16) return dp[i][started][k];
+
+    ll ans = -1e15;
+
+    if (started) {
+      ans = solve_optimize(i, k - 1, false, nums);
+    }
+
+    ans = max((k % 2 == 0 ? -1 : 1) * k * nums[i] +
+                  solve_optimize(i + 1, k, true, nums),
+              ans);
+
+    if (!started) {
+      ans = max(solve_optimize(i + 1, k, false, nums), ans);
+    }
+    return dp[i][started][k] = ans;
   }
 };
