9415c3542b1ab66f8efecc3575e268553403e637
aucker <aucker@qq.com>
2024-03-16 16:14:32 +0800

    fix: update max profit II solution

--- daily/Mar13.cpp
+++ daily/Mar13.cpp
@@ -208,7 +208,7 @@ class Solution {
     int n = prices.size();
     int start = 0;
     int max = 0;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 1; i < n; ++i) {
       if (prices[i] > start) {
         max += prices[i] - start;
       }
