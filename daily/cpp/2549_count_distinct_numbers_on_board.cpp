92af49b3545010df90553a97c0f5257bd151ceba
aucker <aucker@qq.com>
2024-03-23 11:08:51 +0800

    2549: count distinct numbers on board [E]
    easy math problem

---
+++ daily/cpp/2549_count_distinct_numbers_on_board.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  /**
+   * @brief count distinct numbers on board
+   * 
+   * 
+   * @param n 
+   * @return int 
+   */
+  int distinctIntegers(int n) {
+    return n == 1 ? 1 : n - 1;
+  }
+};
