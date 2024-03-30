99f044c44f276a67e6dd4b4ad8e094bf48ada45f
aucker <aucker@qq.com>
2024-03-30 11:36:55 +0800

    992: subarrays with k diff integers [H]
    Hard, need to redo this. This is a sliding window problem

---
+++ daily/cpp/992_subarrays_w_K_diff_integers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int count(vector<int>& nums, int k) {
+    int n = nums.size();
+    map<int, int> mp;
+
+    int i = 0, j = 0;
+    int c = 0;
+    while (j < n) {
+      mp[nums[j]]++;
+
+      while (i <= j && mp.size() > k) {
+        if (--mp[nums[i]] == 0) mp.erase(nums[i]);
+        i++;
+      }
+
+      c += (j - i + 1);
+      j++;
+    }
+    return c;
+  }
+
+  int subArraysWithKDistinct(vector<int>& nums, int k) {
+    return count(nums, k) - count(nums, k - 1);
+  }
+};
