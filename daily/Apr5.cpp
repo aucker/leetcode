5e224074b01efe7869bb47c40d59c0aa61c9eb41
aucker <aucker2020@gmail.com>
2024-04-05 14:04:02 +0800

    update: flatten the binary tree
    use DFS, recursion, iteration, predecessor

--- daily/Apr5.cpp
+++ daily/Apr5.cpp
@@ -61,3 +61,90 @@ public:
 		return ans;
 	}
 };
+
+class FlatBT {
+	void preorderRecur(TreeNode* root, vector<TreeNode*> &l) {
+		if (root) {
+			l.push_back(root);
+			preorderRecur(root->left, l);
+			preorderRecur(root->right, l);
+		}
+	}
+
+public:
+	void flatten(TreeNode* root) {
+		vector<TreeNode*> l;
+		preorderRecur(root, l);
+		int n = l.size();
+		int i = 0;
+		while (i < n) {
+			TreeNode* prev = l.at(i - 1), *curr = l.at(i);
+			prev->left = nullptr;
+			prev->right = curr;
+			i++;
+		}
+	}
+
+	void flattenIter(TreeNode* root) {
+		auto v = vector<TreeNode*> ();
+		auto stk = stack<TreeNode*> ();
+		TreeNode* node = root;
+		while (node || !stk.empty()) {
+			while (node) {
+				v.push_back(node);
+				stk.push(node);
+				node = node->left;
+			}
+			node = stk.top(); stk.pop();
+			node = node->right;
+		}
+		int size = v.size();
+		for (int i = 1; i < size; i++) {
+			auto prev = v.at(i - 1), curr = v.at(i);
+			prev->left = nullptr;
+			prev->right = curr;
+		}
+	}
+
+	void flattenOP(TreeNode* root) {
+		if (!root) return ;
+
+		auto stk = stack<TreeNode*> ();
+		stk.push(root);
+		TreeNode* prev = nullptr;
+		while (!stk.empty()) {
+			TreeNode* curr = stk.top(); stk.pop();
+			if (prev) {
+				prev->left = nullptr;
+				prev->right = curr;
+			}
+
+			TreeNode* left = curr->left, *right = curr->right;
+			if (right) {
+				stk.push(right);
+			}
+			if (left) {
+				stk.push(left);
+			}
+			prev = curr;
+		}
+	}
+
+	void flattenPre(TreeNode* root) {
+		TreeNode* cur = root;
+		while (cur) {
+			if (cur->left) {
+				auto next = cur->left;
+				auto prede = next;
+				while (prede->right) {
+					prede = prede->right;
+				}
+
+				prede->right = cur->right;
+				cur->left = nullptr;
+				cur->right = next;
+			}
+			cur = cur->right;
+		}
+	}
+};
