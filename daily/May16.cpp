9e0427ed1b44075ecaeb973662b775381c8f9c14
aucker <aucker@qq.com>
2024-05-16 11:51:24 +0800

    update: May16, greedy

---
+++ daily/May16.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <numeric>
+
+using std::accumulate;
+using std::max_element;
+using std::vector;
+
+class Solution {
+ public:
+  /**
+   * @brief LC1953: max week numbers you can work
+   *
+   * @param milestones
+   * @return int64_t
+   */
+  auto NumberOfWeeks(vector<int>& milestones) -> int64_t {
+    int64_t s = accumulate(milestones.begin(), milestones.end(), 0LL);
+    int64_t m = *max_element(milestones.begin(), milestones.end());
+    return m > s - m + 1 ? (s - m) * 2 + 1 : s;
+  }
+};
