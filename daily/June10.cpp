ef649fdae5020d638c4d532d811aa0c479a6a2e3
Clang Robot <robot@example.com>
2024-06-11 10:33:31 +0000

    Committing clang-format changes

--- daily/June10.cpp
+++ daily/June10.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 class Solution {
  public:
- /**
-  * @brief LC: 881: Boats to save people
-  * GREEDY: Time: O(nlogn), Space: O(logn)
-  * 
-  * @param people 
-  * @param limit 
-  * @return int 
-  */
+  /**
+   * @brief LC: 881: Boats to save people
+   * GREEDY: Time: O(nlogn), Space: O(logn)
+   *
+   * @param people
+   * @param limit
+   * @return int
+   */
   int numRescueBoats(vector<int>& people, int limit) {
     int len = people.size();
     int le = 0, ri = len - 1;
