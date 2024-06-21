66eed04c86392079a3f2f551a797c87dd0a92d66
aucker <aucker@qq.com>
2024-06-21 21:18:33 +0800

    June21: Trend of temperature change, lambda function [E]
    auto cmp = [](int x, int y) { return (x > y) - (x < y); };
    
    This is a basic lambda function.

---
+++ daily/June21.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  /**
+   * @brief LCP: 61: trend of temperature change [E]
+   * Time: O(N), Space: O(1)
+   *
+   * @param temperatureA
+   * @param temperatureB
+   * @return int
+   */
+  int temperatureTrend(vector<int>& temperatureA, vector<int>& temperatureB) {
+    auto cmp = [](int x, int y) { return (x > y) - (x < y); };
+
+    int ans = 0, same = 0;
+    for (int i = 1; i < temperatureA.size(); i++) {
+      if (cmp(temperatureA[i - 1], temperatureA[i]) ==
+          cmp(temperatureB[i - 1], temperatureB[i])) {
+        ans = max(ans, ++same);
+      } else {
+        same = 0;
+      }
+    }
+    return ans;
+  }
+};
